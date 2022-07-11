b00e21b8fa1dc9f0a4d08b8ac1a9dea261a3ec36
Desmond Kyeremeh <elDekyfin@gmail.com>
2022-07-11 02:43:14 +0000

    Install docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
 tag=0.1
 
+# Install dependencies
+apt update -y
+apt install -y wget
+wget  -O - https://get.docker.com/ | bash
+
 # build
-docker build -t elDekyfin/omv .
+docker build -t eldekyfin/omv .
 
 #deploy
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push eldekyfin/omv:$tag
